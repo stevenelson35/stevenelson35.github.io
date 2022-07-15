29f55cdd7bec9b62c3798d4ff79002d45499e3f9
Steve Nelson <steve.john.nelson@hotmail.com>
2022-07-14 23:10:30 -0400

    Add a capitol day post.

--- create-pics/prep_new_images.sh
+++ create-pics/prep_new_images.sh
@@ -3,14 +3,14 @@
 rm -rf ./assets
 
 mkdir assets
-cp -r ./new-pics/ ./assets/.
+cp -r /mnt/c/repos/create-pics/new-pics/ ./assets/.
 mv ./assets/new-pics ./assets/media
 cp -r ./empty-pic-size-folders/* ./assets/media/.
 
 python3 ./createresponsiveimages.py
 
-cp -r ./assets ../stevenelson35.github.io/.
-rm ../stevenelson35.github.io/assets/media/processed.md
-rm -r create-pics/assets
+cp -r ./assets ../.
+rm ../assets/media/processed.md
+rm -r assets
 
 echo done.
