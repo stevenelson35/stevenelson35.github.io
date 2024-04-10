e55b44e88ce36aa523dda974649af8d5cdff0746
Steve Nelson <steve.john.nelson@hotmail.com>
2024-04-10 05:33:39 -0400

    add spring break 2024 map and base page.

--- create-pics/prep_new_images.sh
+++ create-pics/prep_new_images.sh
@@ -4,13 +4,13 @@ rm -rf ./assets
 
 mkdir assets
 cp -r /mnt/c/repos/create-pics/new-pics/ ./assets/.
-mv ./assets/new-pics ./assets/media
-cp -r ./empty-pic-size-folders/* ./assets/media/.
+mv ./assets/new-pics ./assets/images
+cp -r ./empty-pic-size-folders/* ./assets/images/.
 
 python3 ./createresponsiveimages.py
 
 cp -r ./assets ../.
-rm ../assets/media/processed.md
+rm ../assets/images/processed.md
 rm -r assets
 
 echo done.
---
+++ create-pics/prep_new_media.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf ./assets
+
+mkdir assets
+cp -r /mnt/c/repos/create-pics/new-pics/ ./assets/.
+mv ./assets/new-pics ./assets/media
+cp -r ./empty-pic-size-folders/* ./assets/media/.
+
+python3 ./createresponsivemedia.py
+
+cp -r ./assets ../.
+rm ../assets/media/processed.md
+rm -r assets
+
+echo done.
